754084e076adabfe724e3bb22139085f18b0ae57
shanicebrown <sbrown30@csu.edu>
2020-11-19 19:20:49 -0600

    This is an 8 ball app

--- 8Ball/ViewController.swift
+++ 8Ball/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let ballArray =  ["ball1", "ball2", "ball3", "ball4", "ba115"]
+    
+    var randomBallIndex : Int = 0
 
+    @IBOutlet weak var ballImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
     }
 
-
+    @IBAction func askButtonPressed(_ sender: Any) {
+        
+        newBallImage()
+    }
+    
+    func newBallImage() {
+        
+        randomBallIndex = Int.random(in: 0...4)
+        ballImageView.image = UIImage(named: ballArray[randomBallIndex])
+    }
+    
+    
+    
 }
 
